2cd322cb5fa547d6d4e27c048be0fc56b59da9c0
Eunseong Ko <dmsdl261@naver.com>
2024-03-18 15:27:19 +0900

    s

--- inference.py
+++ inference.py
@@ -1,6 +1,6 @@
 from transformers import Pix2StructForConditionalGeneration, Pix2StructProcessor
-model = Pix2StructForConditionalGeneration.from_pretrained("google/pix2struct-infographics-vqa-large").to("cuda")
-processor = Pix2StructProcessor.from_pretrained("google/pix2struct-infographics-vqa-large")
+model = Pix2StructForConditionalGeneration.from_pretrained("google/pix2struct-infographics-vqa-base").to("cuda")
+processor = Pix2StructProcessor.from_pretrained("google/pix2struct-infographics-vqa-base")
 
 def get_answer(image, question):
     # image = Image.open("/home/ges/level3-cv-productserving-cv-10/data/infographics/images/36919.jpeg")
